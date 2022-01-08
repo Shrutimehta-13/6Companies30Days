4798ee7e55480710fd3d9ec572db0dfde7146087
Shrutimehta-13 <71557910+Shrutimehta-13@users.noreply.github.com>
2022-01-08 20:21:13 +0530

    Update Question5.cpp

--- Goldman Sachs/Question5.cpp
+++ Goldman Sachs/Question5.cpp
@@ -10,7 +10,55 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
+	// #define ull unsigned long long// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
 	// #define ull unsigned long long
+	/* Function to get the nth ugly number*/
+	
+	ull getNthUglyNo(int n) {
+	    // code here
+	    int c2=0, c3=0, c5=0;
+	    vector <ull> dp(n+1);
+	    dp[0]=1;
+	    for(int i=1; i<=n; i++)
+	    {
+	        dp[i] = min({2*dp[c2], 3*dp[c3], 5*dp[c5]});
+	        if(2*dp[c2]==dp[i])
+	            c2++;
+	        if(3*dp[c3]==dp[i])
+	            c3++;
+	        if(5*dp[c5]==dp[i])
+	            c5++;
+	    }
+	    return dp[n-1];
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.getNthUglyNo(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
 	/* Function to get the nth ugly number*/
 	int maxDivide(int a, int b)
 	{
