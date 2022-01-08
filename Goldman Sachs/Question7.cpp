2cf494b7709243ffe8ebcbc35926a828e71c4d70
Shrutimehta-13 <71557910+Shrutimehta-13@users.noreply.github.com>
2022-01-08 19:42:57 +0530

    Update Question7.cpp

--- Goldman Sachs/Question7.cpp
+++ Goldman Sachs/Question7.cpp
@@ -11,10 +11,15 @@ class Solution {
   public:
     int findPosition(int N , int M , int K) {
         // code here
-        if((K+M-1)%N==0)
+        if(M<=N-K+1)
+            return (M+K-1);
+        
+        M = M - (N-K+1);
+
+        if(M%N==0)
             return N;
         else
-        	return (K+M-1)%N;
+            return M%N;
     }
 };
 
